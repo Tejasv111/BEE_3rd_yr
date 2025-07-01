f23d65e96bf26ee77d34089d8eb09f5ce3ab7a85
Tejasv <145493365+Tejasv111@users.noreply.github.com>
2025-07-01 21:16:21 +0530

    Lec 1

---
+++ lecture1/Node.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    return a+b
+}
+function sub(a,b){
+    return a-b
+}
+function multiply(a,b){
+    return a*b
+}
+fs.readfile("demo.txt","utf-8",()=>{
+    console.log(data);
+})
+add(2,3);
+sub(5,6);
+multiply(3,2);
+console.log("exit");
