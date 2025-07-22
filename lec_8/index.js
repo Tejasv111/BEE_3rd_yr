429eb7d99ff3dc216bafc1f7a635ebdcd7904e37
Tejasv <145493365+Tejasv111@users.noreply.github.com>
2025-07-22 06:55:44 +0530

    lec 8

---
+++ lec_8/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
+
+})
+app.get("/watch",(req,res)=>{
+    let videoId= req.query.v;
+    let nId = req.query.n;
+
+    res.send("id got it"+" "+videoId+" "+nId)
+})
+app.get("/watch",(req,res)=>{
+    console.log(req.params.v)
+    console.log(req.params.n)
+    res.send("got it !!")
+    let nId = req.query.n;
+
+    res.send("id got it"+" "+videoId+" "+nId)
+})
+
+app.listen(3000,function(){
+    console.log('started')
+});
+uthhy
